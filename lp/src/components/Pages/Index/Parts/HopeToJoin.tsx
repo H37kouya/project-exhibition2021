5fc2c575d25896d43aec17622dad223ba1334d2b
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2021-11-06 22:01:33 +0900

    feat 1106 TETSU

--- lp/src/components/Pages/Index/Parts/HopeToJoin.tsx
+++ lp/src/components/Pages/Index/Parts/HopeToJoin.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react"
+
+interface Advertise {
+  url: string
+  text: string
+}
+const advertises: Advertise[] = [
+  {url: '', text: 'U-labの広告'},
+  {url: '', text: 'UUADの広告'}
+]
 export const HopeToJoin = () => {
+  const [hello, setHello] = useState(0)
+  useEffect(() => {
+    console.log(hello);
+    console.log(advertises.length);
+    setHello(
+      Math.floor( Math.random() * advertises.length )
+    )
+  }, [])
   return (
-    <div className="bg-blue-200 border-t-8 border-gray-50" style={{ paddingTop: '3rem', paddingBottom: '3rem' }}>
+    <div className="bg-green-100 border-t-8 border-gray-50" style={{ paddingTop: '3rem', paddingBottom: '3rem' }}>
       <div className="text-center">
         <p>
-          <a href="https://forms.gle/1GFM8bLRLn9XHz887" className="font-bold">
-            入部希望は<span className="text-blue-600 underline">こちらから</span>
+          <a href={advertises[hello].url} className="font-bold">
+            {advertises[hello].text}
           </a>
         </p>
       </div>
