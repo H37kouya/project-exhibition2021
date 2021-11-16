646ee55f8c0b211c721e0a3c93895125522120e3
H37kouya <h37kouya@gmail.com>
2021-11-16 22:43:01 +0900

    feature 展示会のイベント内容追加

--- lp/src/components/Pages/Index/Parts/ActivityContents.tsx
+++ lp/src/components/Pages/Index/Parts/ActivityContents.tsx
@@ -6,21 +6,21 @@ export const ActivityContents = () => {
   return (
     <div className="flex justify-center mb-20">
       <div style={{ maxWidth: 1000 }}>
-        <LpHeader title="活動内容" />
+        <LpHeader title="イベント内容" />
 
         <div className="md:grid grid-cols-2 gap-6">
           <ActivityContent
-            title="不定期で誰でも開催・参加することができる勉強会"
+            title="Cafe ink Blue"
             num="01"
-            description="U-labでは誰でも自由に、勉強したことや興味のあることを教え合う勉強会を開催したり、他の人の開いた勉強会に参加したりすることができます。"
-            imagePath="/images/lp1.png"
+            description="プロジェクト展の参加団体がポスターを展示します。ポスターの近くに行くと活動秘話をたくさん聞けるはず！ジャンル不問のLT会もあり、個性が光るメンバーの姿が見れます。"
+            imagePath="/images/cafe_ink_blue.jpg"
           />
 
           <ActivityContent
-            title="不定期で誰でも開催・参加することができる勉強会"
+            title="オリオンスクエア"
             num="02"
-            description="U-labでは、チームで協力して様々な面白いことを企てるチームプロジェクトというものがあります。大会やイベントに参加することもあります。"
-            imagePath="/images/lp2.png"
+            description="オリオン通りの象徴で Polyzome を展示します。Polyzome は生と死をテーマに、「生命とは何か」を表現します。夜は綺麗にライトアップして幻想的な世界が広がります。"
+            imagePath="/images/polyzome_210203_8_0.jpg"
           />
         </div>
       </div>
@@ -44,14 +44,14 @@ const ActivityContent: VFC<Props> = ({
     <div id={`activity_content_${num}`} style={{ maxWidth: 320 }} className="pb-8">
       <div>
         <div className="flex justify-center">
-          <Image src={imagePath} alt={title} width="250" height="250" />
+          <Image src={imagePath} alt={title} className="rounded-lg" width="250" height="250" objectFit="cover" />
         </div>
 
         <div className="flex mt-8">
           <p className="mr-4 text-4xl font-bold">{num}</p>
 
           <div>
-            <h3 className="mb-2 font-bold">{title}</h3>
+            <h3 className="mt-1 mb-2 text-2xl font-bold">{title}</h3>
             <p className="text-sm leading-relaxed">{description}</p>
           </div>
         </div>
