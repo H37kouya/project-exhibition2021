3b7c4c7d4144935e00d2ec77fbc530eacd80b7e9
H37kouya <h37kouya@gmail.com>
2021-11-20 22:34:07 +0900

    update 峰が丘地域貢献ファンド 支援事業の追加

--- lp/src/components/Pages/Index/Parts/HopeToJoin.tsx
+++ lp/src/components/Pages/Index/Parts/HopeToJoin.tsx
@@ -16,17 +16,17 @@ export const HopeToJoin = () => {
     )
   }, [])
   return (
-    <div className='flex' style={{ justifyContent: 'center' , marginBottom: '40px' }}>
-      <a href={advertises[hello].url} className="block bg-green-100 text-center relative ad" style={{ paddingTop: '3rem', paddingBottom: '3rem', borderRadius: '40% 70% 30% 40%/30% 50% 50% 50%' }}>
-        {/*<div className="text-center">
-          <p>
-            <a href={advertises[hello].url} className="font-bold">
-              {advertises[hello].text}
-            </a>
-          </p>
-        </div>*/}
+    <div className='flex' style={{ justifyContent: 'center', marginBottom: '40px' }}>
+      <a
+        href={advertises[hello].url}
+        className="block relative text-center bg-green-100 ad"
+        style={{
+          paddingTop: '3rem',
+          paddingBottom: '3rem',
+          borderRadius: '40% 70% 30% 40%/30% 50% 50% 50%'
+        }}>
         <p className='font-bold font-lg'>
-              {advertises[hello].text}
+          {advertises[hello].text}
         </p>
       </a>
     </div>
